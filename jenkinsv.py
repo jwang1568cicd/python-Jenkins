a7d9257b463ef916036dda77d0a47f42fa33f0a3
JWang1568CICD <jwang1568cicd2024@outlook.com>
2024-04-04 15:21:38 -0700

    added json.dump

--- jenkinsv.py
+++ jenkinsv.py
@@ -1,7 +1,13 @@
 import jenkins
+import json
 
 server = jenkins.Jenkins('http://192.168.5.5:8080', username='admin', password='admin')
 user = server.get_whoami()
 version = server.get_version()
 print('Hello %s from Jenkins %s' % (user['fullName'], version))
+
+print('Jenkins plugins:')
+plugins = server.get_plugins_info()
+#print(plugins)
+json.dumps(plugins)
 ~
